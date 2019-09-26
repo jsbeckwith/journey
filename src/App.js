c15e7d5c6f099c133e2e01a0ac07c25930cbb0ee
juliaslohman <juliaslohman@gmail.com>
2019-09-26 14:21:20 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-// import css
+import './App.css';
 
 function App() {
   return (
-    <title>asdf</title>
+    <div>asdf</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import css
-import App from './journey';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
