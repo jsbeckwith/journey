ea11ce51588bd57ae66f736e1036a4d4337d8c78
juliaslohman <juliaslohman@gmail.com>
2019-10-03 15:49:40 -0700

    new-entry header start on header, scss variables

--- src/newEntryPage/newEntryPage.js
+++ src/newEntryPage/newEntryPage.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import logo from '../logo.svg';
 import './newEntryPage.scss';
-import TextEditor from './textEditor.js'
-import SubmitSaveButton from './submitSaveButton.js'
+import TextEditor from './textEditor.js';
+import NewEntryHeader from './newEntryHeader.js';
 
 class NewEntryPage extends React.Component {
 	constructor (props) {
 		super(props);
+		// at some point this will have a prop that determines if the page is
+		// editing an existing entry or making an entirely new one
+		// which changes discard/delete and the title
 	}
 
 	render () {
 		return (
 			<div>
-				{/* header component will go here */}
+				<NewEntryHeader/>
 				<TextEditor/>
-				<SubmitSaveButton/>
 			</div>
 		);
 	}
