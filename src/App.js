4a73a0157e0d77f33ac9a2886a78cf2322548fe5
mimilouie <33444956+mimilouie@users.noreply.github.com>
2019-10-08 15:08:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ class App extends React.Component {
 	}
 
 	render() {
-		let todayDate = new Date().toDateString();
+		let date = new Date();
+		const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		const months = ["January", "February", "March", "April", "May", "June",
+  											"July", "August", "September", "October", "November", "December"];
+		// create a string with the full day of the week, month, day of the month, and year
+		let todayDate = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate()
+										+ ", " + date.getFullYear();
 
 		return (
 			// for now the app just renders the homepage body but later
 			// we'll add logic to determine which page should be loaded i think
 			// navbar will also go here
 			<div>
+				// pass todayDate as a prop to NewEntryPage
 				<NewEntryPage date={todayDate}/>
 			</div>
 		);
