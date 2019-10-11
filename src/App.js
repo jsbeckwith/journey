60985a5d52027b78babca953de01a9ea14e8a6b0
juliaslohman <juliaslohman@gmail.com>
2019-10-10 21:12:43 -0700

    navbar first pass, css incomplete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import NewEntryPage from './newEntryPage/newEntryPage.js';
 import Ribbon from './ribbon.js';
+import Nav from './nav.js';
+import NewEntryPage from './newEntryPage/newEntryPage.js';
 import './universalStyle.scss';
 
 class App extends React.Component {
@@ -13,17 +14,20 @@ class App extends React.Component {
 		let date = new Date();
 		const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 		const months = ["January", "February", "March", "April", "May", "June",
-  											"July", "August", "September", "October", "November", "December"];
+  						"July", "August", "September", "October", "November", "December"];
 		// create a string with the full day of the week, month, day of the month, and year
 		let todayDate = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate()
-										+ ", " + date.getFullYear();
-
+						+ ", " + date.getFullYear();
+		
 		return (
 			// for now the app just renders the homepage body but later
 			// we'll add logic to determine which page should be loaded i think
 			<div>
+				<Nav/>
 				<Ribbon/>
-				<NewEntryPage date={todayDate}/>
+				<div class="page-body">
+					<NewEntryPage date={todayDate}/>
+				</div>
 			</div>
 		);
 	}
