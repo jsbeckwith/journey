7bb18b171de2b32b985511a9ac45332bb4673237
auhnix <hava.xinhua@gmail.com>
2019-11-04 23:32:41 -0800

    working on post-save redirect

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // open our connection to the mongodb database!
+/*const uri = "mongodb+srv://auhnix:<Db2admin!>@journey-c0-paelm.gcp.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;*/
+
 mongoose.connect('mongodb://127.0.0.1:27017/posts', { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 
