8b6d14937ee861caf14c45c1b28fb2f2491591d7
Mimi <angeline.louie@gmail.com>
2019-11-14 23:47:18 -0800

    loginpage, routes, logout button

---
+++ src/buttons/logoutButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+
+class LogoutButton extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Tooltip title="logout">
+        <button className="logoutButton log-button" onClick={() => {alert('logout'); window.location = "http://localhost:3000/"}}>
+          Logout
+        </button>
+      </Tooltip>
+    );
+  }
+
+}
+
+export default LogoutButton;
